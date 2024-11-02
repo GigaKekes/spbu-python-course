7f68cfdec3293971e5655794225a04f26bd644ae
Dmitry Klimovsky <dmitryklimovsky05@gmail.com>
2024-11-02 13:07:45 +0300

    added more sophisticated strategy

--- project/game/examples/main.py
+++ project/game/examples/main.py
@@ -16,10 +16,33 @@ if __name__ == "__main__":
         lambda: {"type": "color", "choice": random.choice(["Red", "Black", "Green"])},
     )
 
+    def multy_strategy():
+        r = random.randint(0, 6)
+        if r == 0:
+            return {"type": "color", "choice": random.choice(["Red", "Black", "Green"])}
+        if r == 1:
+            return {"type": "parity", "choice": random.choice(["Even", "Odd"])}
+        if r == 2:
+            return {"type": "number", "choice": random.randint(1, 36)}
+        if r == 3:
+            return {"type": "number", "choice": 0}
+        if r == 4:
+            return {"type": "row", "choice": random.randint(1, 3)}
+        if r == 5:
+            return {"type": "half", "choice": random.randint(1, 2)}
+        if r == 6:
+            return {"type": "third", "choice": random.randint(1, 3)}
+
+    Bot.add_strategy(
+        "Schizo bet",
+        multy_strategy,
+    )
+
     bots = [
         Bot("Vasya", "Numerical bet"),
         Bot("Kolya", "Parity bet"),
         Bot("Misha", "Color bet"),
+        Bot("Oleg", "Schizo bet"),
         CrazyBot("Dmitry", "Color bet"),
     ]
     Croupier.max_rounds = 10
